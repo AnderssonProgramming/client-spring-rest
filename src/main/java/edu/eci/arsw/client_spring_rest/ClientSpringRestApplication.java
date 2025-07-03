0d9d6eb1a990d7e78b435f7e3e9fcf9c6f0bf807
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2025-07-02 19:33:41 -0500

    feat: add client spring rest scaffolding project

---
+++ src/main/java/edu/eci/arsw/client_spring_rest/ClientSpringRestApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.client_spring_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arsw/client_spring_rest/ClientSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.client_spring_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
